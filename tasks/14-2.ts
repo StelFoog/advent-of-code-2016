1bb17f435ec81288d6a6317dabc250e3a52c20a0
Samuel Larsson <samuel@familjenlarsson.eu>
2024-11-28 22:18:19 +0100

    Day 14

---
+++ tasks/14-2.ts
@@ -0,0 +1,34 @@
+import { input } from "../src/inputManager";
+
+const hasher = new Bun.CryptoHasher("md5");
+
+let matches = 0;
+const matched: { char: string; idx: number; matchedWith: number }[] = [];
+
+let lim = Number.MAX_SAFE_INTEGER;
+for (let i = 0; i < lim; i++) {
+	const hash = hashRounds(`${input}${i}`);
+	const threeMatch = /(.)\1\1/.exec(hash);
+	if (!threeMatch) continue;
+
+	for (let j = 0; j <= hash.length - 5; j++) {
+		const curr = hash[j];
+		if (hash.slice(j, j + 5) !== curr.repeat(5)) continue;
+		for (const match of matched) {
+			if (match.char !== curr || i - match.idx > 1000 || match.matchedWith) continue;
+			matches++;
+			match.matchedWith = i;
+		}
+	}
+	if (matches >= 64 && lim === Number.MAX_SAFE_INTEGER) lim = i + 1_000;
+	matched.push({ char: threeMatch[1], idx: i, matchedWith: 0 });
+}
+console.log(matched.filter((v) => v.matchedWith)[63].idx);
+
+function hashRounds(str: string) {
+	let hash = str;
+	for (let i = 0; i <= 2016; i++) {
+		hash = hasher.update(hash).digest("hex");
+	}
+	return hash;
+}
